a3da7bf51113219c7a67b54f0799f2aff3d1731d
AlexanderGhosh <ghoshalexander@gmail.com>
2021-11-27 17:40:43 +0000

    back to 0auth2

--- src/libs/email.js
+++ src/libs/email.js
@@ -1,34 +1,20 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-const OAuth2 = google.auth.OAuth2;
-
-const oauth2Client = new OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  'https://developers.google.com/oauthplayground'
-);
-
-/*oauth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN
-});
-
-const accessToken = oauth2Client.getAccessToken()*/
 
 const smtpTransport = nodemailer.createTransport({
   service: "gmail",
 
-    auth: {
-      user: process.env.EMAIL_USERNAME, // generated ethereal user
-      pass: process.env.EMAIL_PASSWORD, // generated ethereal password
-    },
   /*auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },*/
+  auth: {
     type: "OAuth2",
     user: process.env.EMAIL_USERNAME,
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     refreshToken: process.env.GOOGLE_REFRESH_TOKEN
-  },*/
+  },
   tls: {
     rejectUnauthorized: false
   }
